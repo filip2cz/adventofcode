1072272ad1a961917893f72fb7cc581563871689
Filip Komárek <filip2cz>
2023-12-02 19:00:45 +0100

    ADD: first day - part 1

---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+string filePath = "C:\\Users\\filip\\git\\adventofcode\\1\\input.txt";
+
+string[] input = File.ReadAllLines(filePath);
+
+string[] onlyNumbers = new string[input.Length];
+
+int i = 0;
+foreach (string thing in input)
+{
+    onlyNumbers[i++] = Regex.Replace(thing, "[^0-9]", "");
+}
+
+int output = 0;
+
+foreach (string thing in onlyNumbers)
+{
+    output = output + Convert.ToInt16($"{thing.Substring(0, 1)}{thing[thing.Length - 1]}");
+}
+
+Console.WriteLine(output);
